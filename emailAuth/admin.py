fc31310675fdf1cf8f18d807fb3cede7f23d5958
Yuroparo <142691050+Yuroparo@users.noreply.github.com>
2025-03-29 20:25:17 +0100

    Delete emailAuth directory

--- emailAuth/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User, Voice, TTSConversion, UserHistory, Payment
-
-admin.site.register(User)
-admin.site.register(Voice)
-admin.site.register(TTSConversion)
-admin.site.register(UserHistory)
-admin.site.register(Payment)
